8c4fe5569fe48ea34a20e72ca03aa2c4b928e757
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-17 18:06:08 +0600

    chgein

--- src/components/Pages/Home/FeatureAll.js
+++ src/components/Pages/Home/FeatureAll.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const FeatureAll = () => {
     return (
-        <div class="flex items-center justify-between mb-12">
-            <h4 class="mt-2 text-xl font-bold">Book List</h4>
+        <div className="flex items-center justify-between mb-12">
+            <h4 className="mt-2 text-xl font-bold">Book List</h4>
 
-            <div class="flex items-center space-x-4">
-                <Link to='/'><button class="lws-filter-btn active-filter">All</button></Link>
-                <Link to='/featureComponent'><button class="lws-filter-btn">Featured</button></Link>
+            <div className="flex items-center space-x-4">
+                <Link to='/'><button className="lws-filter-btn active-filter">All</button></Link>
+                <Link to='/featureComponent'><button className="lws-filter-btn">Featured</button></Link>
 
             </div>
         </div>
