ecce32924207e511f855cc558a195debf0fbfa55
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-17 18:05:05 +0600

    chagein

--- src/components/Pages/Edit/EditBookComponent.js
+++ src/components/Pages/Edit/EditBookComponent.js
@@ -22,10 +22,10 @@ const EditBookComponent = () => {
 
     return (
         <div>
-            <main class="py-6 2xl:px-6">
-                <div class="container">
-                    <div class="p-8 overflow-hidden bg-white shadow-cardShadow rounded-md max-w-xl mx-auto">
-                        <h4 class="mb-8 text-xl font-bold text-center">Edit Book</h4>
+            <main className="py-6 2xl:px-6">
+                <div className="container">
+                    <div className="p-8 overflow-hidden bg-white shadow-cardShadow rounded-md max-w-xl mx-auto">
+                        <h4 className="mb-8 text-xl font-bold text-center">Edit Book</h4>
                         {content}
                     </div>
                 </div>
--- src/components/Pages/Edit/Form.js
+++ src/components/Pages/Edit/Form.js
@@ -54,59 +54,59 @@ const Form = ({ book }) => {
     }
     return (
 
-        <form onSubmit={handleSubmit} class="book-form">
-            <div class="space-y-2">
+        <form onSubmit={handleSubmit} className="book-form">
+            <div className="space-y-2">
                 <label for="lws-bookName">Book Name</label>
                 <input
                     value={name}
                     onChange={(e) => setName(e.target.value)}
-                    required class="text-input" type="text" id="lws-bookName" name="name" />
+                    required className="text-input" type="text" id="lws-bookName" name="name" />
             </div>
 
-            <div class="space-y-2">
+            <div className="space-y-2">
                 <label for="lws-author">Author</label>
                 <input
                     value={author}
                     onChange={(e) => setAuthor(e.target.value)}
-                    required class="text-input" type="text" id="lws-author" name="author" />
+                    required className="text-input" type="text" id="lws-author" name="author" />
             </div>
 
-            <div class="space-y-2">
+            <div className="space-y-2">
                 <label for="lws-thumbnail">Image Url</label>
                 <input
                     value={thumbnail}
                     onChange={(e) => setThumbnail(e.target.value)}
-                    required class="text-input" type="text" id="lws-thumbnail" name="thumbnail" />
+                    required className="text-input" type="text" id="lws-thumbnail" name="thumbnail" />
             </div>
 
-            <div class="grid grid-cols-2 gap-8 pb-4">
-                <div class="space-y-2">
+            <div className="grid grid-cols-2 gap-8 pb-4">
+                <div className="space-y-2">
                     <label for="lws-price">Price</label>
                     <input
                         value={price}
                         onChange={(e) => setPrice(e.target.value)}
-                        required class="text-input" type="number" id="lws-price" name="price" />
+                        required className="text-input" type="number" id="lws-price" name="price" />
                 </div>
 
-                <div class="space-y-2">
+                <div className="space-y-2">
                     <label for="lws-rating">Rating</label>
                     <input
                         value={rating}
                         onChange={(e) => setRating(e.target.value)}
-                        required class="text-input" type="number" id="lws-rating" name="rating" min="1"
+                        required className="text-input" type="number" id="lws-rating" name="rating" min="1"
                         max="5" />
                 </div>
             </div>
 
-            <div class="flex items-center">
+            <div className="flex items-center">
                 <input id="lws-featured"
                     value={featured}
                     onChange={handleFeature}
-                    type="checkbox" name="featured" class="w-4 h-4" />
-                <label for="lws-featured" class="ml-2 text-sm"> This is a featured book </label>
+                    type="checkbox" name="featured" className="w-4 h-4" />
+                <label for="lws-featured" className="ml-2 text-sm"> This is a featured book </label>
             </div>
 
-            <button type="submit" class="submit" id="lws-submit">Edit Book</button>
+            <button type="submit" className="submit" id="lws-submit">Edit Book</button>
         </form>
 
     );
